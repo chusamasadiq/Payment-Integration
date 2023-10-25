1bc34534c5ddd05961862da728b8eabef8c9fcb0
Usama Sadiq <usamasadiq163@gmail.com>
2023-10-25 20:38:58 +0500

    payment

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterPayment());
+}
+
+class FlutterPayment extends StatelessWidget {
+  const FlutterPayment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
